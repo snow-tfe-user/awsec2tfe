5f64456263567ee7221f172f8023dccba34761e6
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-09-05 17:52:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@ provider "aws" {
   secret_key          = "${var.secret_key}"
   allowed_account_ids = ["${var.first_account_id}"]
   region              = "${var.region}"
-  version             = ">= 3.0"
+  version             = ">= 2.0"
 }
